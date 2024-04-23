7ff21732871bef91de8c0163ff2639742fef2755
Marielsy Carvajal <carvajalmarielsy@gmail.com>
2024-04-23 17:23:31 -0400

    dashboard querys

--- app/src/main/java/com/example/mynotesapp2/ui/ViewModel/DashboardViewModel.kt
+++ app/src/main/java/com/example/mynotesapp2/ui/ViewModel/DashboardViewModel.kt
@@ -1,8 +1,10 @@
 package com.example.mynotesapp2.ui.ViewModel
 
+import androidx.lifecycle.ViewModel
+import com.example.mynotesapp2.data.Model.remote.ApiService
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class DashboardViewModel @Inject constructor() {
+class DashboardViewModel @Inject constructor(private val repository: ApiService) : ViewModel(){
 }
